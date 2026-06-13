b66dac27d20cdb341a7c9db410e480ebd26aaa67
Bill <bill@thewallisfamily.co.uk>
2026-06-13 20:18:00 +0100

    chore: run pre-commit

--- barcode_generator/ean.py
+++ barcode_generator/ean.py
@@ -5,11 +5,12 @@ EAN-8 and EAN-13 barcode generator.
 from __future__ import annotations
 
 import dataclasses
+from typing import Any
 
 
 @dataclasses.dataclass
 class BitArray(list):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
     def __str__(self) -> str:
